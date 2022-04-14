b33af2b7c7f5054435dc876e52d6dbe485535a80
J.Cole <ccjohn015@gmail.com>
2022-04-14 08:00:47 -0500

    added hooks

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+//import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
@@ -7,6 +8,10 @@ function Nav(props) {
     setCurrentCategory,
     currentCategory,
   } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
